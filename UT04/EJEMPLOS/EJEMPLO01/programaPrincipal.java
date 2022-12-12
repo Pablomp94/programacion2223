3d24f41c6f507efcd10dce318d2bf954cab33c36
Pablomp94 <114667642+Pablomp94@users.noreply.github.com>
2022-12-12 13:57:09 +0100

    Delete UT04/EJEMPLOS directory

--- UT04/EJEMPLOS/EJEMPLO01/programaPrincipal.java
+++
@@ -1,7 +0,0 @@
-public class programaPrincipal {
-    public static void main(String[] args) {
-        coche miToyota = new coche();
-        miToyota.repostar(30);
-        miToyota.conducir(300);
-    }
-}
